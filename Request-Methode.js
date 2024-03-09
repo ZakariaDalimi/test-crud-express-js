f7445be9190d9a836253224be537ee1aad5de8fd
ZakariaDalimi <zakariaa.dalimi@gmail.com>
2024-03-09 16:25:00 +0100

    Add Get Api

---
+++ Request-Methode.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+// Get METHODE 
+app.get("/", (req, res) => {
+  console.log('GET Request Successfull!')
+  res.send("Got a Put request");
+});
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+
+
+
+
+
+
+
+// PUT  : katms7 ligne kaml w tmodifoer w t3awd t3mrr ligne
+
+// Patch   :  katmodifier gha daq column

dfa53770dec230ab605a06107c02c47b5bc1e083
ZakariaDalimi <zakariaa.dalimi@gmail.com>
2024-03-09 15:54:27 +0100

    Add Get App

---
+++ app-get.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  console.log('GET Request Successfull!')
+  res.send("Got a Put request");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+
+
+// PUT  : katms7 ligne kaml w tmodifoer w t3awd t3mrr ligne
+
+// Patch   :  katmodifier gha daq column
